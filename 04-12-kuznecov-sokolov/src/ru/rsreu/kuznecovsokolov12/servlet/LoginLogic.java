3b3528721cc376daa218932103b81516db0d17a5
Egor Sokolov <86056646+faositb@users.noreply.github.com>
2023-11-15 11:30:22 +0300

    Update LoginLogic.java

--- 04-12-kuznecov-sokolov/src/ru/rsreu/kuznecovsokolov12/servlet/LoginLogic.java
+++ 04-12-kuznecov-sokolov/src/ru/rsreu/kuznecovsokolov12/servlet/LoginLogic.java
@@ -46,8 +46,9 @@ public class LoginLogic {
 		Sanction lastSanction = sanctionDAO.getLastUserSanction(user);
 //		System.out.println(lastSanction.getType().getName());
 		if (lastSanction.getReceiver() != null) {
-			if (lastSanction.getType().toString().equals("Block")) {
+			if (lastSanction.getType().getName().toString().equals("Block")) {
 				System.out.println("user is blocked");
+				factory.returnConnectionToPool();
 				return EnumLogin.NOUSER;
 			}
 		}
