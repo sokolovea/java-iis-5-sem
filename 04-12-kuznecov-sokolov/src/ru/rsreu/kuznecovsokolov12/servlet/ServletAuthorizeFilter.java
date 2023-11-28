32e609b198411b4db7f63b5499122fd954e2e32f
Egor Sokolov <86056646+faositb@users.noreply.github.com>
2023-11-29 01:28:41 +0300

    Add Filter from Blinov

--- 04-12-kuznecov-sokolov/src/ru/rsreu/kuznecovsokolov12/servlet/ServletAuthorizeFilter.java
+++ 04-12-kuznecov-sokolov/src/ru/rsreu/kuznecovsokolov12/servlet/ServletAuthorizeFilter.java
@@ -12,13 +12,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import ru.rsreu.kuznecovsokolov12.datalayer.DAOFactory;
-import ru.rsreu.kuznecovsokolov12.datalayer.DBType;
-import ru.rsreu.kuznecovsokolov12.datalayer.TeamDAO;
-import ru.rsreu.kuznecovsokolov12.datalayer.TeamInteractDAO;
-import ru.rsreu.kuznecovsokolov12.datalayer.UserDAO;
-import ru.rsreu.kuznecovsokolov12.datalayer.data.User;
-
 public class ServletAuthorizeFilter implements Filter {
 	private String indexPath;
 
@@ -28,53 +21,19 @@ public class ServletAuthorizeFilter implements Filter {
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		if (true) {
-			chain.doFilter(request, response);
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse resp = (HttpServletResponse) response;
+		HttpSession session = req.getSession();
+		EnumLogin type = (EnumLogin) session.getAttribute(ActionCommand.PARAM_USER_ROLE);
+		System.out.println("Сейчас мы в фильтре на NOUSER");
+		if (type == null) {
+			System.out.println("Фильтр на NOUSER сработал");
+			type = EnumLogin.NOUSER;
+			session.setAttribute(ActionCommand.PARAM_USER_ROLE, type);
+			RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher("/jsp/index.jsp");
+			dispatcher.forward(req, resp);
 			return;
 		}
-		HttpServletRequest httpRequest = (HttpServletRequest) request;
-		HttpServletResponse httpResponse = (HttpServletResponse) response;
-		
-		String pathInfo = httpRequest.getPathInfo();
-		System.out.print("PATHINFO = " + pathInfo);
-        if (pathInfo == null || pathInfo.equals("/login")) {
-            chain.doFilter(request, response);
-            return;
-        }
-        
-		HttpSession session = httpRequest.getSession(false);
-		if (session == null) {
-			System.out.println("SESSION == NULL!!!");
-			RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/index.jsp");
-			dispatcher.forward(request, response);
-			return;
-		}
-		String login = (String) session.getAttribute(ActionCommand.PARAM_USER_LOGIN);
-		String loginRequest = request.getParameter(LoginCommand.PARAM_USER_LOGIN);
-		if (login == null && loginRequest == null) {
-			System.out.println("LOGIN == NULL!");
-			httpResponse.sendRedirect(httpRequest.getContextPath() + "/login");
-			return;
-		}
-		DAOFactory factory = DAOFactory.getInstance(DBType.ORACLE);
-		UserDAO userDAO = factory.getUserDAO();
-		User user = null;
-		try {
-			user = userDAO.getUserByLogin(login);
-			if (!(user.isIsAuthorized())) {
-//				RequestDispatcher dispatcher = request.getRequestDispatcher("/login");
-
-				// Forward к целевому ресурсу
-//				dispatcher.forward(request, response);
-				httpResponse.sendRedirect(httpRequest.getContextPath() + "/login");
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		httpResponse.sendRedirect(httpRequest.getContextPath());
 		chain.doFilter(request, response);
-	}
-
-	public void destroy() {
 	}
 }
